a92e0ad93aa5e6a63b026844a6368d91a1f705e5
rohitsingh122 <71955815+rohitsingh122@users.noreply.github.com>
2020-11-01 20:13:40 +0530

    class_basics1

---
+++ class1.py
@@ -0,0 +1,14 @@
+class Employee:
+    def __init__(self,name,salary,role):
+        self.name = name
+        self.salary = salary
+        self.role = role
+
+e = Employee("rohit",3444,"student")
+print(e.name,e.salary,e.role)
+
+
+
+
+    
+
