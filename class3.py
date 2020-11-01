ac10e1ae5bf8f0fc83e5bdd687eb36be293f8bb9
rohitsingh122 <71955815+rohitsingh122@users.noreply.github.com>
2020-11-01 20:50:08 +0530

    static_variable_or_class_variable

---
+++ class3.py
@@ -0,0 +1,20 @@
+class student:
+    college = "ULCA"
+    def __init__(self,name,rollno):
+        self.name = name
+        self.rollno = rollno
+    def display(self):
+        print("student name:",self.name,"\n""student college:",d)
+        print("student rollno:",self.rollno,"\n""student college:",d)
+
+s = student("rohit",34)
+d = student.college
+s.display()
+
+
+
+
+    
+  
+        
+      
