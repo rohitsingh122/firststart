257516d9e1b83cd7901c931ace3019cd2bd173f8
rohitsingh122 <71955815+rohitsingh122@users.noreply.github.com>
2020-11-03 15:00:24 +0530

    string

--- string.py
+++ string.py
@@ -1,7 +1,41 @@
-name ="EDUREKA"
+name ="edureka"
 print(name[-1:-8:-1])
 print(name[::-1])
 a = "python is a intrepreted language"
 print(a[5:])
 print(a[0:15:2])
 print(a[::-1 ])
+print(a.capitalize()) #for making first word capital
+print(a.count("p")) #count how many times a word is reapted
+print(a.find("p")) # to find the pos of value
+print(a.isalnum())
+print(a.upper())
+print(a.replace("p","$")) # takes two arguments 
+print(a.strip()) #basically return the clean version of string
+print(a.split("a"))#splits the string into parts according to user 
+
+#operations on ptyhon
+
+a = "python"
+b = "ruby"
+print(a+b)#concatination of two strings
+print((a+b)*2)
+
+#string interpolation
+#string interploation is a method which makes is easier for developers to execute string formatting and concatination in python
+
+# % formatting
+c = "you can learn %s easily, and %s too" %(a,b)
+print(c)
+
+# f string 
+
+d =f'you can learn {a} and {b} easily'
+print(d)
+
+#string.format
+
+e = "you can easily learn{a} and {b}".format(a=a,b=b)
+print(e)
+
+#templetes strings
